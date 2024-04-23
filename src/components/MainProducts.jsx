2d137da7aaae6e3875e9559b72ff40247ecf6614
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-04-23 19:28:37 +0000

    Responsive now

--- src/components/MainProducts.jsx
+++ src/components/MainProducts.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 const MainProducts = () => {
   return (
     <>
-        
+        <div className="container px-4 py-5 ht-100-real">
+            <h2 className='pb-2 border-bottom'>Featured Products</h2>
+            <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4  py-5">
+                <div className="">1</div>
+                <div className="">2</div>
+                <div className="">3</div>
+                <div className="">4</div>
+                <div className="">5</div>
+                <div className="">6</div>
+            </div>
+        </div>
     </>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+import MainProducts from '../components/MainProducts'
 
 const HomePage = () => {
   return (
     <>
-        <div className="">
+        <div className="w-100">
             <Navbar />
             <Hero />
+            <MainProducts />
         </div>
     </>
   )
