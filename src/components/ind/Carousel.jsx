25d980d025c9fe1888ec97e38944b3707f36c502
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-04-23 19:48:19 +0000

    Another push

--- src/components/ind/Carousel.jsx
+++ src/components/ind/Carousel.jsx
@@ -5,7 +5,7 @@ const Carousel = ({ header, text, button }) => {
     <div class="container col-xxl-8 ht-100 px-4">
     <div class="row flex-lg-row-reverse align-items-center g-1 py-1">
       <div class="col-10 col-sm-8 col-lg-6">
-        <img src="https://placehold.co/600x400" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
+        <img className='' src="https://placehold.co/600x400" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
       </div>
       <div class="col-lg-6">
         <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">{header}</h1>
