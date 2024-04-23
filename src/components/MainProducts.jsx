bcc8d82e4884b49d7b859bf56f11b89355764ad9
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-04-23 15:47:54 +0000

    New mod

---
+++ src/components/MainProducts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainProducts = () => {
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default MainProducts
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ import Hero from '../components/Hero'
 const HomePage = () => {
   return (
     <>
-        <div className="container-xxl">
+        <div className="">
             <Navbar />
             <Hero />
         </div>
