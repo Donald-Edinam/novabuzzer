0ad4676ef1cf80ffe81ccf55e9590a6bdb422e62
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-04-23 20:51:54 +0000

    Another pusg ahanin:

--- src/components/MainProducts.jsx
+++ src/components/MainProducts.jsx
@@ -10,8 +10,13 @@ const MainProducts = () => {
             <h2 className='pb-2 border-bottom'>Featured Products</h2>
             <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4  py-5">
                 {demoArray.map((item) => (
-                    <div className="col">
-                        
+                    <div className="col card card-cover flex flex-column">
+                        <h3 className="card-header text-center fw-bold">
+                            Quality Arts
+                        </h3>
+                        <div className="card-body">
+                            <p className="card-text text-center">Professional touch</p>
+                        </div>
                     </div>
                 ))}
             </div>
